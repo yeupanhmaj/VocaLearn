82d470683fc568186d6bab290f4bc83024738baf
Hoàng <wallker.night@gmail.com>
2019-10-14 21:39:28 +0700

    Firtcommit

---
+++ app/src/main/java/com/example/vocalearn/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package com.example.vocalearn.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
